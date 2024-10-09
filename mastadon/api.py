96b2ac3b57968640d20f62905af2ed551cf2452c
aaryanneupane <aaryan200513@gmail.com>
2024-10-08 20:21:11 -0700

    feat: Added package requirements and fetched timeline data from mastodon

---
+++ mastadon/api.py
@@ -0,0 +1,15 @@
+from mastodon import Mastodon
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+access_token = os.getenv("ACCESS_TOKEN")
+
+mastodon = Mastodon(
+    access_token=access_token,
+    api_base_url='https://mastodon.social'
+)
+
+public_timeline = mastodon.timeline_public()
+for toot in public_timeline:
+    print(toot['content'])
